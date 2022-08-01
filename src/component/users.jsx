a515890fc15d455b88cf229567336ff4e2309989
Kirill Romanov <kirillromanov@MacBook-Pro-Kirill.local>
2022-08-01 22:41:02 +0300

    pagination

--- src/component/users.jsx
+++ src/component/users.jsx
@@ -1,32 +1,49 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import User from "./user";
+import Pagination from "./pagination";
+import { paginate } from "../app/utils/paginate";
 
-const Users = ({ users, handleDelete, ...rest }) => {
+const Users = ({ users, ...rest }) => {
+  const count = users.length;
+  const pageSize = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+  const handlePageChange = (pageIndex) => {
+    setCurrentPage(pageIndex);
+  };
 
-    return (
-        <>
-            {users.length !== 0 ? (
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
-                        <th scope="col">Профессия</th>
-                        <th scope="col">Встретились, раз</th>
-                        <th scope="col">Оценка</th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {users.map((user) => (
-                        <User key={user._id} {...user} handleDelete={handleDelete} {...rest}/>
-                    ))}
-                    </tbody>
-                </table>
-            ):("")}
-        </>
-    );
+  const userCrop = paginate(users, currentPage, pageSize);
+
+  return (
+    <>
+      {count > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретились, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col">Избранное</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {userCrop.map((user) => (
+              <User {...rest} {...user} key={user._id} />
+            ))}
+          </tbody>
+        </table>
+      )}
+
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
+  );
 };
 
 export default Users;
